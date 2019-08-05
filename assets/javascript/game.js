743ac8717116ffcb5bed111fc6419ce3b2f0cff1
Jim Chung <jchung225@gmail.com>
2019-08-05 19:16:19 -0400

    made the css and javascript link to html and added color to html

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+var winsText = "";
+var loseText="";
+var GuessSoFar="";
+var GuessLeft="";
+var userGuess ="";
+var ComputerGuess = "";
+
+// declare buttons
+var button1
+var button2
+var button3
+var button4
+
+
+// game counters
+var winscount = 0;
+var losecount=0;
+var numberOfGuesses = 9;
+
+function startgame (){
+
+
+
+
+
+
+
+
+
+
+    
+}
