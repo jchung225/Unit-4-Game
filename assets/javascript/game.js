de523900656eb5170c513bb532505376cef7c5bd
Jim Chung <jchung225@gmail.com>
2019-08-05 20:08:06 -0400

    made the css and starting on javascripting added reset css also

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,36 +2,32 @@
 
 
 
-
-var winsText = "";
-var loseText="";
-var GuessSoFar="";
-var GuessLeft="";
+// grabbing elements
+var winsText = document.getElementById("Wins");
+var loseText = document.getElementById("Losses");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
 var userGuess ="";
 var ComputerGuess = "";
 
-// declare buttons
-var button1
-var button2
-var button3
-var button4
+// randomizer for computer and buttons
+computerGuess = computerChoices[Math.floor(Math.random();
 
 
 // game counters
 var winscount = 0;
 var losecount=0;
 var numberOfGuesses = 9;
+var totalScore=0;
 
 function startgame (){
 
 
+}
+
+function reset(){
 
 
-
-
-
-
-
-
-    
 }
